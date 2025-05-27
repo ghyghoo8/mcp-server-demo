91842fe5dd02ab0f65249be25d16d22ace0f5bdd
ghyghoo8 <ghyghoo8@gmail.com>
2025-05-27 16:24:10 +0800

    feat: save

--- README.md
+++ README.md
@@ -2,6 +2,13 @@
 写一个mcp服务
 
 
+python 运行
+
+```
+开发：uvicorn ./py/main:app --reload   
+生产：uvicorn ./py/main:app
+
+```
 
 ### 资料
 * demo1：https://github.com/MCP-100/stock-market-server/blob/main/src/index.ts
---
+++ py/main.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+from  Ashare import *
+
+class Params(BaseModel):
+    frequency: str
+    count: int
+    end_date: str | None = None
+
+app = FastAPI()
+
+@app.post("/query/{code}")
+def query_code(code:str, query: Params):
+    df=get_price(code, frequency=query.frequency, count=query.count, end_date=query.end_date) 
+    data = df.to_dict(orient="records")
+    return { "code": 200, "data": data }
+
+@app.get("/")
+def read_root():
+  return {"code": 200 }
